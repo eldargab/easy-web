8cf29373fb2eef0747dea2a8c63b465cbe321982
Eldar Gabdullin <eldargab@gmail.com>
2013-03-24 22:04:34 +0400

    more benchmarks for comparison

---
+++ bench/pure-http.js
@@ -0,0 +1,18 @@
+var App = require('..')
+var http = require('http')
+var request = App.http.Request
+var send = App.http.send
+var Router = App.Router
+var Route = App.Route
+
+var router = new Router
+
+router.push(new Route('GET', '/', 'hello'))
+
+http.createServer(function (req, res) {
+  req.__proto__ = request
+
+  router.dispatch(req.path, req)
+
+  send(req, res, 'Hello world')
+}).listen(8000)
